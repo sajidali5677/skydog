166270c0f61e4c49b05d65749f5e09d6f7c4b512
mohammad sajid ali <sa4868091@gmail.com>
2023-08-20 23:10:33 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,66 @@
+gsap.from(".l3",{
+    x:400,
+    delay:0.5,
+    duration:1,
+    opacity:0.2
+    
+})
+gsap.from(".l2",{
+    x:300,
+    delay:0.6,
+    duration:1,
+    
+})
+gsap.from(".l1",{
+    x:200,
+    delay:0.7,
+    stagger:0.3,
+    duration:1,
+    
+})
+gsap.from(".c",{
+    scale:0,
+    delay:0.7,
+    stagger:0.3,
+    duration:1,
+    
+})
+gsap.from(".r1",{
+    x:-200,
+    delay:0.7,
+    duration:1,
+    
+})
+gsap.from(".r2",{
+    x:-300,
+    delay:0.6,
+    duration:1,
+    
+})
+gsap.from(".r3",{
+    x:-400,
+    delay:0.5,
+    duration:1,
+    opacity:0.2
+    
+})
+gsap.from(".p1bottom h1",{
+    y:300,
+    delay:.2,
+    duration:1
+})
+gsap.from(".navimg ",{
+    x:-300,
+    delay:.5,
+    duration:.5
+
+})
+gsap.from(".navbtn ",{
+    x:300,
+    delay:.5,
+    duration:.5,
+
+
+})
+
+
